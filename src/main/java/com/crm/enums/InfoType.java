4ce74d8ed571a0a1225aa4fe077a31ac0a63fe62
Ben wu <845687624@qq.com>
2018-08-08 19:40:38 +0800

    Update info type and add toString method

--- src/main/java/com/crm/enums/InfoType.java
+++ src/main/java/com/crm/enums/InfoType.java
@@ -5,9 +5,26 @@ package com.crm.enums;
  * 2018/8/8 11:23
  **/
 public enum InfoType {
-    TRAVEL(1), BROADCAST(2), BARGAIN(3);
-    private int index;
-    InfoType(int index){
-        this.index = index;
+    TRAVEL(1,"商旅咨询"), BROADCAST(2,"通告"), BARGAIN(3,"特价信息");
+    private int code;
+    private String message;
+    InfoType(int code,String message){
+        this.code = code;
+        this.message = message;
+    }
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 }
