6e9014f7054387d06592429b33c7e53f07958501
Ben wu <845687624@qq.com>
2018-08-08 19:42:40 +0800

    modified Testing method of ClientService Test

--- src/test/java/com/crm/service/client/ClientServiceTest.java
+++ src/test/java/com/crm/service/client/ClientServiceTest.java
@@ -1,5 +1,29 @@
+package com.crm.service.client;
+
+import com.crm.entity.Customer;
+import com.crm.entity.Info;
+import com.crm.entity.User;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
 import static org.junit.Assert.*;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class ClientServiceTest {
 
+    @Autowired
+    private ClientService clientService;
+    @Test
+    public void findTravelInfo() {
+        Customer user = new Customer();
+        user.setId(1L);
+        List<Info> list = clientService.findTravelInfo(user);
+        System.out.print(list);
+    }
 }
