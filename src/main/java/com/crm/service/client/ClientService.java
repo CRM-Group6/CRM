7d17650ff4416f7a851b53b737b42bee99dcdec1
Ben wu <845687624@qq.com>
2018-08-08 19:39:31 +0800

    Update ClientService class

--- src/main/java/com/crm/service/client/ClientService.java
+++ src/main/java/com/crm/service/client/ClientService.java
@@ -1,9 +1,35 @@
 package com.crm.service.client;
 
 
+import com.crm.entity.Customer;
+import com.crm.entity.Info;
+import com.crm.entity.User;
+import com.crm.enums.InfoType;
+import com.crm.mapper.InfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class ClientManageService {
+public class ClientService {
+
+    @Autowired
+    private InfoMapper infoMapper;
+    //查看商旅咨询
+    public List<Info> findTravelInfo(Customer customer){
+        return infoMapper.selectInfoByUserID(customer.getId(),InfoType.TRAVEL.getCode());
+    }
+    //查看特价信息
+    public List<Info> findBarginInfo(Customer customer){
+        return null;
+    }
+    //查看通告
+
+    //查看用户订单
+
+    //查看用户账单
+
+    //联系客服人员
 
 }
