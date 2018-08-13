78eb7e9021106a84a5fec5e1a26e6da8c6a5a3d5
godjohncc <825546086@qq.com>
2018-08-13 17:46:50 +0800

    修改路径，增加页面

--- src/main/java/com/crm/controller/custom_service/ProblemLibraryController.java
+++ src/main/java/com/crm/controller/custom_service/ProblemLibraryController.java
@@ -1,7 +1,9 @@
 package com.crm.controller.custom_service;
 
+import com.crm.VO.ResultVO;
 import com.crm.entity.ProblemLibrary;
 import com.crm.service.custom_service.ProblemLibraryService;
+import com.crm.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,8 +16,9 @@ public class ProblemLibraryController {
     @Autowired
     private ProblemLibraryService problemLibraryService;
     @GetMapping("/find/{id}")
-    public ProblemLibrary findone(@PathVariable("id") Long id){
-            return problemLibraryService.findone(id);
+    public ResultVO findone(@PathVariable("id") Long id)
+    {
+            return ResultVOUtil.success( problemLibraryService.findone(id));
     }
 
 }
