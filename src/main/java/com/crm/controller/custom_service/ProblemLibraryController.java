7222f3a7018f7d2465ca6ee550ea728af65eafdc
godjohncc <825546086@qq.com>
2018-08-14 16:02:47 +0800

    增加footable插件

--- src/main/java/com/crm/controller/custom_service/ProblemLibraryController.java
+++ src/main/java/com/crm/controller/custom_service/ProblemLibraryController.java
@@ -5,19 +5,19 @@ import com.crm.entity.ProblemLibrary;
 import com.crm.service.custom_service.ProblemLibraryService;
 import com.crm.utils.ResultVOUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/problem")
+
 public class ProblemLibraryController {
     @Autowired
     private ProblemLibraryService problemLibraryService;
-    @GetMapping("/find/{id}")
-    public ResultVO findone(@PathVariable("id") Long id)
-    {
+    @GetMapping("console/financial_management")
+    public ResultVO findone(@RequestParam("selectrequest") Long id)
+    {       if(problemLibraryService.findone(id)==null){
+        return ResultVOUtil.error();
+        }
+        else
             return ResultVOUtil.success( problemLibraryService.findone(id));
     }
 
