e12b42a1dab2e948b5b2e8ac56b071f754f8a1f9
Ben wu <845687624@qq.com>
2018-08-20 17:53:27 +0800

    客户分析界面完成，饼状图完成

--- src/main/java/com/crm/controller/client/CustomerAnalysisController.java
+++ src/main/java/com/crm/controller/client/CustomerAnalysisController.java
@@ -1,6 +1,10 @@
 package com.crm.controller.client;
 
 import com.crm.VO.ShowSum;
+import com.crm.VO.chart.Chart;
+import com.crm.VO.chart.ChartVO;
+import com.crm.VO.chart.Pie;
+import com.crm.VO.chart.PieItem;
 import com.crm.entity.Customer;
 import com.crm.service.client.CustomerAnalysis;
 import com.crm.utils.ResultVOUtil;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -28,8 +33,18 @@ public class CustomerAnalysisController {
     private ModelAndView customerRegionAnalysis(){
         List<ShowSum> list = customerAnalysis.customerRegionAnalysis();
         ModelAndView model= new ModelAndView("/client/customer_analysis");
+        List<PieItem> dataList = new ArrayList<>();
+        for (int i=0;i<list.size();i++){
+            PieItem pieItem = new PieItem(list.get(i).getMsg(),list.get(i).getNum());
+            dataList.add(pieItem);
+        }
+        Pie chart = new Pie("区域分析饼图",dataList);
+        chart.setType("pie");
+        List<Pie> charts = new ArrayList<>();
+        charts.add(chart);
+        ChartVO chartVO = new ChartVO(charts,null,null,null,"区域分析");
         if(list!=null){
-            return model.addObject("result",ResultVOUtil.success(list));
+            return model.addObject("result",ResultVOUtil.success(chartVO));
         }else
             return model.addObject("result",ResultVOUtil.error());
     }
@@ -38,8 +53,18 @@ public class CustomerAnalysisController {
     private ModelAndView customerSourceAnalysis(){
         List<ShowSum> list = customerAnalysis.customerSourceAnalysis();
         ModelAndView model= new ModelAndView("/client/customer_analysis");
+        List<PieItem> dataList = new ArrayList<>();
+        for (int i=0;i<list.size();i++){
+            PieItem pieItem = new PieItem(list.get(i).getMsg(),list.get(i).getNum());
+            dataList.add(pieItem);
+        }
+        Pie chart = new Pie("区域分析饼图",dataList);
+        chart.setType("pie");
+        List<Pie> charts = new ArrayList<>();
+        charts.add(chart);
+        ChartVO chartVO = new ChartVO(charts,null,null,null,"来源分析");
         if(list!=null){
-            return model.addObject("result",ResultVOUtil.success(list));
+            return model.addObject("result",ResultVOUtil.success(chartVO));
         }else
             return model.addObject("result",ResultVOUtil.error());
     }
@@ -48,8 +73,18 @@ public class CustomerAnalysisController {
     private ModelAndView customerIndustryAnalysis(){
         List<ShowSum> list = customerAnalysis.customerIndustryAnalysis();
         ModelAndView model= new ModelAndView("/client/customer_analysis");
+        List<PieItem> dataList = new ArrayList<>();
+        for (int i=0;i<list.size();i++){
+            PieItem pieItem = new PieItem(list.get(i).getMsg(),list.get(i).getNum());
+            dataList.add(pieItem);
+        }
+        Pie chart = new Pie("区域分析饼图",dataList);
+        chart.setType("pie");
+        List<Pie> charts = new ArrayList<>();
+        charts.add(chart);
+        ChartVO chartVO = new ChartVO(charts,null,null,null,"行业分析");
         if(list!=null){
-            return model.addObject("result",ResultVOUtil.success(list));
+            return model.addObject("result",ResultVOUtil.success(chartVO));
         }else
             return model.addObject("result",ResultVOUtil.error());
     }
